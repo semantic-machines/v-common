4f1aea32ac2ff71bd833fa4831f7caea28ddb991
itiua <ValeriyBushenev@gmail.com>
2024-04-18 18:50:05 +0300

    add acl-cache-indexes

--- src/az_impl/az_lmdb.rs
+++ src/az_impl/az_lmdb.rs
@@ -9,12 +9,14 @@ use v_authorization::common::{Storage, Trace};
 use v_authorization::*;
 
 const DB_PATH: &str = "./data/acl-indexes/";
+const CACHE_DB_PATH: &str = "./data/acl-cache-indexes/";
 
 use crate::az_impl::stat_manager::StatPub;
 use crate::module::module_impl::Module;
 
 pub struct LmdbAzContext {
     env: Environment,
+    cache_env: Option<Environment>,
     authorize_counter: u64,
     max_authorize_counter: u64,
     stat: Option<StatPub>,
@@ -31,8 +33,21 @@ fn open(max_read_counter: u64, stat_collector_url: Option<String>) -> LmdbAzCont
         match env_builder.open(DB_PATH, 0o644) {
             Ok(env) => {
                 info!("LIB_AZ: Opened environment {}", DB_PATH);
+                let cache_env_builder = EnvBuilder::new().flags(EnvCreateNoLock | EnvCreateReadOnly | EnvCreateNoMetaSync | EnvCreateNoSync);
+                let cache_env = match cache_env_builder.open(CACHE_DB_PATH, 0o644) {
+                    Ok(env) => {
+                        info!("LIB_AZ: Opened cache environment {}", CACHE_DB_PATH);
+                        Some(env)
+                    },
+                    Err(e) => {
+                        warn!("Authorize: Err opening cache environment: {:?}. Proceeding without cache.", e);
+                        None
+                    },
+                };
+
                 return LmdbAzContext {
                     env,
+                    cache_env,
                     authorize_counter: 0,
                     max_authorize_counter: max_read_counter,
                     stat,
@@ -100,7 +115,7 @@ impl AuthorizationContext for LmdbAzContext {
             }
         }
 
-        match _f_authorize(&mut self.env, uri, user_uri, request_access, _is_check_for_reload, trace, &mut self.stat) {
+        match self.authorize_use_db(uri, user_uri, request_access, _is_check_for_reload, trace) {
             Ok(r) => {
                 return Ok(r);
             },
@@ -119,23 +134,41 @@ impl AuthorizationContext for LmdbAzContext {
                 }
             },
         }
-        _f_authorize(&mut self.env, uri, user_uri, request_access, _is_check_for_reload, trace, &mut self.stat)
+        // retry authorization if db err
+        self.authorize_use_db(uri, user_uri, request_access, _is_check_for_reload, trace)
     }
 }
 
 pub struct AzLmdbStorage<'a> {
     db: &'a Database<'a>,
+    cache_db: Option<&'a Database<'a>>,
     stat: &'a mut Option<StatPub>,
 }
 
 impl<'a> Storage for AzLmdbStorage<'a> {
     fn get(&mut self, key: &str) -> io::Result<Option<String>> {
+        if let Some(cache_db) = self.cache_db {
+            match cache_db.get::<String>(&key) {
+                Ok(val) => {
+                    if let Some(p) = self.stat {
+                        p.collect(key);
+                    }
+                    return Ok(Some(val));
+                },
+                Err(e) => match e {
+                    MdbError::NotFound => {
+                        // Данные не найдены в кеше, продолжаем чтение из основной базы
+                    },
+                    _ => return Err(Error::new(ErrorKind::Other, format!("Authorize: cache_db.get {:?}, {}", e, key))),
+                },
+            }
+        }
+
         match self.db.get::<String>(&key) {
             Ok(val) => {
                 if let Some(p) = self.stat {
                     p.collect(key);
                 }
-
                 Ok(Some(val))
             },
             Err(e) => match e {
@@ -148,34 +181,48 @@ impl<'a> Storage for AzLmdbStorage<'a> {
     fn fiber_yield(&self) {}
 }
 
-fn _f_authorize(
-    env: &mut Environment,
-    uri: &str,
-    user_uri: &str,
-    request_access: u8,
-    _is_check_for_reload: bool,
-    trace: &mut Trace,
-    stat: &mut Option<StatPub>,
-) -> Result<u8, std::io::Error> {
-    let db_handle = match env.get_default_db(DbFlags::empty()) {
-        Ok(db_handle_res) => db_handle_res,
-        Err(e) => {
-            return Err(Error::new(ErrorKind::Other, format!("Authorize: Err opening db handle: {:?}", e)));
-        },
-    };
+impl LmdbAzContext {
+    fn authorize_use_db(&mut self, uri: &str, user_uri: &str, request_access: u8, _is_check_for_reload: bool, trace: &mut Trace) -> Result<u8, std::io::Error> {
+        let db_handle = match self.env.get_default_db(DbFlags::empty()) {
+            Ok(db_handle_res) => db_handle_res,
+            Err(e) => {
+                return Err(Error::new(ErrorKind::Other, format!("Authorize: Err opening db handle: {:?}", e)));
+            },
+        };
+        let txn = match self.env.get_reader() {
+            Ok(txn1) => txn1,
+            Err(e) => {
+                return Err(Error::new(ErrorKind::Other, format!("Authorize:CREATING TRANSACTION {:?}", e)));
+            },
+        };
+        let db = txn.bind(&db_handle);
 
-    let txn = match env.get_reader() {
-        Ok(txn1) => txn1,
-        Err(e) => {
-            return Err(Error::new(ErrorKind::Other, format!("Authorize:CREATING TRANSACTION {:?}", e)));
-        },
-    };
+        let txn_cache;
+        let cache_db = if let Some(env) = &self.cache_env {
+            let db_handle = match env.get_default_db(DbFlags::empty()) {
+                Ok(db_handle_res) => db_handle_res,
+                Err(e) => {
+                    return Err(Error::new(ErrorKind::Other, format!("Authorize: Err opening db handle: {:?}", e)));
+                },
+            };
+            txn_cache = match env.get_reader() {
+                Ok(txn1) => txn1,
+                Err(e) => {
+                    return Err(Error::new(ErrorKind::Other, format!("Authorize:CREATING TRANSACTION {:?}", e)));
+                },
+            };
+            let cache_db = txn_cache.bind(&db_handle);
+            Some(cache_db)
+        } else {
+            None
+        };
 
-    let db = txn.bind(&db_handle);
-    let mut storage = AzLmdbStorage {
-        db: &db,
-        stat: stat,
-    };
+        let mut storage = AzLmdbStorage {
+            db: &db,
+            cache_db: cache_db.as_ref(),
+            stat: &mut self.stat,
+        };
 
-    authorize(uri, user_uri, request_access, &mut storage, trace)
+        authorize(uri, user_uri, request_access, &mut storage, trace)
+    }
 }
