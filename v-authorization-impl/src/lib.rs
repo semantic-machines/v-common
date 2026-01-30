6a6e483b6cc64071894fb57d0d816f3bd038f597
itiua <ValeriyBushenev@gmail.com>
2026-01-30 15:24:38 +0300

    downgrade v-authorization-impl to "0.2.2"

--- v-authorization-impl/src/lib.rs
+++ v-authorization-impl/src/lib.rs
@@ -2,16 +2,8 @@
 extern crate log;
 
 pub mod az_lmdb;
-#[cfg(feature = "tt")]
-pub mod az_tarantool;
-#[cfg(feature = "tt")]
-mod runtime_wrapper;
-pub mod az_context;
 mod stat_manager;
 
 pub use az_lmdb::LmdbAzContext;
-#[cfg(feature = "tt")]
-pub use az_tarantool::TarantoolAzContext;
-pub use az_context::AzContext;
 pub use v_authorization;
 
